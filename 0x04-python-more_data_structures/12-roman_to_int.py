791643f42424d7bfddc625622f053ec2773da147
Abigiya Haile <bgyabigiya95@gmail.com>
2022-07-06 13:34:46 -0700

    commit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
 
 
-def uniq_add(my_list=[]):
+def roman_to_int(roman_string):
     """
-    A function that adds all unique
-    integers in a list (only once for each integer)
+    A function that converts a Roman numeral to an integer.
     """
-    new_list = []
-    sum = 0
-    for num in my_list:
-        if num not in new_list:
-            sum += num
-            new_list.append(num)
-        return sum
+    num_map = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    i = 0
+    total = 0
+    if isinstance(roman_string, str):
+        for i in range(len(roman_string) - 1):
+            if num_map[roman_string[i]] >= num_map[roman_string[i + 1]]:
+                total += num_map[roman_string[i]]
+            else:
+                total -= num_map[roman_string[i]]
+            i += 1
+        total += num_map[roman_string[i]]
+        return total
+    return 0
